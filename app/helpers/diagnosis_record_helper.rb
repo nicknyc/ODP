56b711550d8b0a0c6b831ee879b06783bbb354d0
Lily Sattagowit <buffbear13@gmail.com>
2016-11-08 19:53:27 +0700

    Add Appointment, DiagnosisRecord controller and view

---
+++ app/helpers/diagnosis_record_helper.rb
@@ -0,0 +1,2 @@
+module DiagnosisRecordHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'diagnosis_record/index'
+
+  get 'diagnosis_record/new'
+
+  get 'diagnosis_record/edit'
+
+  get 'diagnosis_record/show'
+
+  get 'appointment/index'
+
+  get 'appointment/edit'
+
+  get 'appointment/new'
+
+  get 'appointment/show'
+
   devise_for :users
   devise_for :models
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
