9f4d23edd1bba335e009530f952645e49faba2cc
Lily Sattagowit <buffbear13@gmail.com>
2016-11-14 16:14:25 +0700

    can now login with either ext_id or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,21 @@ class User < ApplicationRecord
 
   belongs_to :user_type, :polymorphic => true
 
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.ext_id || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["ext_id = :value OR email = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:ext_id) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
 end
