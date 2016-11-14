207d25e582ad0d9a63422955c0dff3d8d38861f2
Lily Sattagowit <buffbear13@gmail.com>
2016-11-14 11:59:20 +0700

    Add ban user cannot log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :banned?
+
+ def banned?
+   if current_user.present? && current_user.ban?
+     sign_out current_user
+     flash[:error] = "This account has been suspended"
+     root_path
+   end
+ end
 end
