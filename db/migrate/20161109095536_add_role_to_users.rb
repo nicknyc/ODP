0d8c6586fd12d4f19bb5ef41eb4dc94e5cf526cf
Pakkapol <Pakkapol1994@gmail.com>
2016-11-17 15:58:08 +0700

    make compatible with sqlite3

--- db/migrate/20161109095536_add_role_to_users.rb
+++ db/migrate/20161109095536_add_role_to_users.rb
@@ -1,7 +1,7 @@
 class AddRoleToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :user_type_type, "ENUM('Admin','Patient','Staff','Doctor','Nurse','Pharmacist')" ,  null: false
-    add_column :users, :user_type_id, :integer, :null => false
+      add_column :users, :user_type_type, :string
+    add_column :users, :user_type_id, :integer
 
     add_index :users, [:user_type_type, :user_type_id], :unique => true
     add_index :users, :user_type_type
--- db/schema.rb
+++ db/schema.rb
@@ -12,48 +12,48 @@
 
 ActiveRecord::Schema.define(version: 20161117012846) do
 
-  create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "appointments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "status",                           default: "Pending"
-    t.datetime "created_at",                                           null: false
-    t.datetime "updated_at",                                           null: false
-    t.text     "diagnosis_record",   limit: 65535
-    t.text     "presciption",        limit: 65535
+  create_table "appointments", force: :cascade do |t|
+    t.string   "status",             default: "Pending"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.text     "diagnosis_record"
+    t.text     "presciption"
     t.integer  "patient_id"
     t.integer  "doctor_id"
     t.integer  "physical_record_id"
     t.integer  "schedule_id"
-    t.text     "symptom",            limit: 65535
+    t.text     "symptom"
   end
 
-  create_table "doctors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "doctors", force: :cascade do |t|
     t.string   "proficiency"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  create_table "nurses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "nurses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "patients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "allergyRecordList", limit: 65535
+  create_table "patients", force: :cascade do |t|
+    t.text     "allergyRecordList"
     t.string   "bloodType"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
-  create_table "pharmacists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "pharmacists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "physical_records", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "physical_records", force: :cascade do |t|
     t.integer  "weight"
     t.integer  "height"
     t.string   "bloodPressure"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20161117012846) do
     t.integer  "appointment_id"
   end
 
-  create_table "schedules", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "schedules", force: :cascade do |t|
     t.integer  "doctor_id"
     t.integer  "shift"
     t.date     "date"
@@ -72,37 +72,37 @@ ActiveRecord::Schema.define(version: 20161117012846) do
     t.datetime "updated_at",              null: false
   end
 
-  create_table "staffs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "staffs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                             default: "",    null: false
-    t.string   "encrypted_password",                default: "",    null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                     default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
-    t.string   "user_type_type",         limit: 10,                 null: false
-    t.integer  "user_type_id",                                      null: false
-    t.integer  "national_id",                                       null: false
-    t.string   "sex",                    limit: 1,                  null: false
-    t.string   "first_name",                                        null: false
-    t.string   "last_name",                                         null: false
-    t.integer  "phone_number",                                      null: false
-    t.boolean  "ban",                               default: false, null: false
-    t.integer  "ext_id",                            default: -1
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-    t.index ["user_type_type", "user_type_id"], name: "index_users_on_user_type_type_and_user_type_id", unique: true, using: :btree
-    t.index ["user_type_type"], name: "index_users_on_user_type_type", using: :btree
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "user_type_type"
+    t.integer  "user_type_id"
+    t.integer  "national_id"
+    t.string   "sex"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "phone_number"
+    t.boolean  "ban",                    default: false, null: false
+    t.integer  "ext_id",                 default: -1
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_type_type", "user_type_id"], name: "index_users_on_user_type_type_and_user_type_id", unique: true
+    t.index ["user_type_type"], name: "index_users_on_user_type_type"
   end
 
 end
