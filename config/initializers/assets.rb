e4c53714cbf23186537d151ff5192da4dc7829a6
Suraphoj <suraphoj@dmit.co.th>
2016-11-19 18:53:30 +0700

    test layout javascripts still bug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery-2.2.3.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( fastclick.min.js )
+Rails.application.config.assets.precompile += %w( app.min.js )
+Rails.application.config.assets.precompile += %w( jquery.slimscroll.min.js )
