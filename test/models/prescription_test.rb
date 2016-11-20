c96297e1d9b261ceafecdd0031516fde96334f9c
Pakkapol <Pakkapol1994@gmail.com>
2016-11-21 02:27:58 +0700

    add prescription_test all test/model committed

--- test/models/prescription_test.rb
+++ test/models/prescription_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class PrescriptionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @prescriptions = Prescription.new(appointment_id: 1,med: "aaa",no: 1)
+  end
+
+  test "prescription can be nil" do
+    @prescriptions = Prescription.new
+    assert @prescriptions.valid?
+  end
+
+  test "prescription test1 should be valid" do
+    assert @prescriptions.valid?
+  end
+
+  test "created_at shouldn't be nil" do
+    @prescriptions.created_at = nil
+    assert_not @prescriptions.valid?
+  end
+
+  test "updated_at shouldn't be nil" do
+    @prescriptions.updated_at = nil
+    assert_not @prescriptions.valid?
+  end
+
+  test "appointment_id shouldn't be nil" do
+    @prescriptions.appointment_id = nil
+    assert_not @prescriptions.valid?
+  end
+
+  test "med shouldn't be nil" do
+    @prescriptions.med = nil
+    assert_not @prescriptions.valid?
+  end
+
+  test "no shouldn't be nil" do
+    @prescriptions.no = nil
+    assert_not @prescriptions.valid?
+  end
+
+  test "no shouldn't be zero" do
+    @prescriptions.no = 0
+    assert_not @prescriptions.valid?
+  end
+
+  test "no shouldn't be negative" do
+    @prescriptions.no = -1
+    assert_not @prescriptions.valid?
+  end
 end
