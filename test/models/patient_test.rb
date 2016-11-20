7cc19e2419067210e6a9ca780b2fb2cae254aef9
Pakkapol <Pakkapol1994@gmail.com>
2016-11-21 01:24:42 +0700

    add patient_test

--- test/models/patient_test.rb
+++ test/models/patient_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class PatientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "patient.yml test1 should be valid" do
+    @patient = patients(:test1)
+    assert @patient.valid?
+  end
+
+  test "patient.new shouldn't be nil" do
+    patient = Patient.new
+    assert_not_nil patient
+  end
+
+  test "created_at shouldn't be nil" do
+    @patient = patients(:test3)
+    assert_not @patient.valid?
+  end
+
+  test "updated_at shouldn't be nil" do
+    @patient = patients(:test2)
+    assert_not @patient.valid?
+  end
+
+  test "allergyRecordList can be nil" do
+    @patient = patients(:test4)
+    assert @patient.valid?
+  end
+
+  test "bloodType shouldn't be nil" do
+    @patient = patients(:test5)
+    assert_not @patient.valid?
+  end
+
+  test "bloodType shouldn't be number" do
+    @patient = patients(:test5)
+    @patient.bloodType = 123
+    assert_not @patient.valid?
+  end
 end
