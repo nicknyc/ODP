cc626a66dafc1e72caefc478e7ca8d8fd9168282
Pakkapol <Pakkapol1994@gmail.com>
2016-11-21 00:10:57 +0700

    physical_record test

--- test/models/physical_record_test.rb
+++ test/models/physical_record_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PhysicalRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @appointment = Appointment.new(doctor_id: 1,patient_id: 1,symptom: "headache")
+  end
+
+  test "nil record should be valid" do
+    assert_nil @appointment.physical_record
+  end
+
+  test "should be valid" do
+    @appointment.physical_record = physical_records(:test1)
+    assert_not_nil @appointment.physical_record.valid?
+    assert @appointment.physical_record.valid?
+  end
 end
