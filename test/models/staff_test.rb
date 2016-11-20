9b5e1981d208a355f877ba37dff049ea3e629886
Pakkapol <Pakkapol1994@gmail.com>
2016-11-21 02:16:59 +0700

    add staff_test

--- test/models/staff_test.rb
+++ test/models/staff_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class StaffTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "staff.yml test1 should be valid" do
+    @staff = staffs(:test1)
+    assert @staff.valid?
+  end
+
+  test "staff.new shouldn't be nil" do
+    staff = Staff.new
+    assert_not_nil staff
+  end
+
+  test "created_at shouldn't be nil" do
+    @staff = staffs(:test3)
+    assert_not @staff.valid?
+  end
+
+  test "updated_at shouldn't be nil" do
+    @staff = staffs(:test2)
+    assert_not @staff.valid?
+  end
 end
