51fbdc5ed805cb9f2a7d592d40b3abc049b4f74e
Pakkapol <Pakkapol1994@gmail.com>
2016-11-20 22:43:12 +0700

    add appointment test

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class AppointmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @appointment = Appointment.new(doctor_id: 1,patient_id: 1,symptom: "headache")
+  end
+
+  test "setup should be valid" do
+    assert @appointment.valid?
+  end
+
+  test "symptom shouldn't be nil" do
+    @appointment.symptom = nil
+    assert_not @appointment.valid?
+  end
+
+  test "doctor_id shouldn't be nil" do
+    @appointment.doctor_id = nil
+    assert_not @appointment.valid?
+  end
+
+  test "patient_id shouldn't be nil" do
+    @appointment.patient_id = nil
+    assert_not @appointment.valid?
+  end
+
 end
--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -1,7 +1,5 @@
 require 'test_helper'
 
 class ScheduleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   
 end
