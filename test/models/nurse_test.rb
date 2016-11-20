f3f93d3864514cf02fe88c39c11c0999632e4e5b
Pakkapol <Pakkapol1994@gmail.com>
2016-11-21 01:11:18 +0700

    add nurse_test

--- test/models/nurse_test.rb
+++ test/models/nurse_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class NurseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "nurse.yml test1 should be valid" do
+    @nurse = nurses(:test1)
+    assert @nurse.valid?
+  end
+
+  test "nurse.new shouldn't be nil" do
+    nurse = Nurse.new
+    assert_not_nil nurse
+  end
+
+  test "created_at shouldn't be nil" do
+    @nurse = nurses(:test3)
+    assert_not @nurse.valid?
+  end
+
+  test "updated_at shouldn't be nil" do
+    @nurse = nurses(:test2)
+    assert_not @nurse.valid?
+  end
 end
