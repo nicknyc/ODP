9cfaaa7a4ea2a8876dbbf7a76e41d31bae5c1824
Pakkapol <Pakkapol1994@gmail.com>
2016-11-22 01:00:46 +0700

    demo milestone

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
+ActiveRecord::Schema.define(version: 20161121094559) do
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -94,23 +95,6 @@
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-<<<<<<< HEAD
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.string   "user_type_type"
-    t.integer  "user_type_id"
-    t.integer  "national_id"
-    t.string   "sex"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.integer  "phone_number"
-    t.boolean  "ban",                    default: false, null: false
-    t.integer  "ext_id",                 default: -1
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["user_type_type", "user_type_id"], name: "index_users_on_user_type_type_and_user_type_id", unique: true
-    t.index ["user_type_type"], name: "index_users_on_user_type_type"
-=======
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
     t.string   "user_type_type",         limit: 10,                 null: false
@@ -126,7 +110,6 @@
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["user_type_type", "user_type_id"], name: "index_users_on_user_type_type_and_user_type_id", unique: true, using: :btree
     t.index ["user_type_type"], name: "index_users_on_user_type_type", using: :btree
->>>>>>> origin/'cleanup'
   end
 
 end
